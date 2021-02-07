7f98678f1b74aff02b1c9aafdba861a72486b838
Andrew Magdy <andrewmagdygad@gmail.com>
2021-02-07 16:01:00 +0200

    fix: add port

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,9 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-console.log(process.env.CONNECTION_STRING);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
